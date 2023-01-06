9a0b25a2bb366390274f29d24c332d7d0c4c04fb
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2023-01-06 12:34:08 +0300

    styles

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,10 @@ export default function Home() {
   return (
     <div>
       <HeroBanner></HeroBanner>
+      <div className="products-heading">
+        <h2>Best Seller Products</h2>
+        <p>speaker There are many variations passages</p>
+      </div>
       <Product></Product>
       <FooterBanner></FooterBanner>
     </div>
