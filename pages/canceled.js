b25b0d02a0cf2dcd67e87f0579829ba487148066
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2023-01-13 10:44:44 +0300

    canceled page

--- pages/canceled.js
+++ pages/canceled.js
@@ -1,29 +1,24 @@
 import React, { useEffect } from "react";
 import Link from "next/link";
-import { BsBagCheckFill } from "react-icons/bs";
+import { ImCancelCircle } from "react-icons/im";
 import { useStateContext } from "../context/StateContext";
-import { randomInRange } from "../lib/utils";
 
-function Success() {
+function Canceled() {
   const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
 
   useEffect(() => {
     setCartItems([]);
     setTotalPrice(0);
     setTotalQuantities(0);
-    randomInRange();
   }, []);
   return (
     <div className="success-wrapper">
       <div className="success">
-        <p className="icon">
-          <BsBagCheckFill />
+        <p className="icon_">
+          <ImCancelCircle />
         </p>
-        <h2>Thank you for your order!</h2>
-        <p className="email-msg">Check your email inbox for the receipt.</p>
-        <p className="email-msg">
-          Your item(s) will be shipped to your address.
-        </p>
+        <h2>Your order has been Canceled!</h2>
+
         <p className="description">
           If you have any questions, please email
           <a className="email" href="nyongesaderrick@gmail.com">
@@ -40,4 +35,4 @@ function Success() {
   );
 }
 
-export default Success;
+export default Canceled;
